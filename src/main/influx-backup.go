72f43cd9427f44cc8024d8b2648c2bde483c6d3a
Timothy Raymond <xtjraymondx@gmail.com>
2015-07-23 15:03:07 -0400

    Add periodic backup capturing

--- src/main/influx-backup.go
+++ src/main/influx-backup.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"runtime"
 	"time"
 
 	"github.com/mitchellh/goamz/aws"
@@ -16,29 +17,72 @@ var (
 	influxHost string = os.Getenv("INFLUXDB_PORT_8808_TCP_ADDR")
 	influxPort string = os.Getenv("INFLUXDB_PORT_8808_TCP_PORT")
 	s3Bucket   string = os.Getenv("AWS_BUCKET")
+	freqStr    string = os.Getenv("BACKUP_FREQUENCY")
 	letters           = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 )
 
 func main() {
+	backupFrequency, err := time.ParseDuration(freqStr)
+	if err != nil {
+		log.Fatalln("Error parsing backup frequency:", err.Error())
+	}
+
+	stop := make(chan struct{})
+	log.Printf("Backups will be taken every %s\n", backupFrequency)
+	ticker := time.NewTicker(backupFrequency)
+	go func() {
+		log.Println("Ready")
+		for {
+			select {
+			case <-ticker.C:
+				log.Println("Starting backup...")
+				backup, err := captureBackup()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Println("Uploading backup to S3...")
+				err = s3Upload("influx-"+randName(10), s3Bucket, backup)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Println("Cleaning up...")
+				info, _ := backup.Stat()
+				err = os.Remove(info.Name())
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Println("Backup Complete")
+			case <-stop:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	for {
+		time.Sleep(10 * time.Second)
+		runtime.Gosched()
+	}
+}
+
+func s3Upload(uploadName string, bucketName string, content *os.File) error {
 	auth, err := aws.EnvAuth()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	client := s3.New(auth, aws.USEast)
-	backupBucket := client.Bucket(s3Bucket)
+	bucket := client.Bucket(bucketName)
 
-	backup, err := triggerBackup()
+	info, err := content.Stat()
 	if err != nil {
-		panic("something went wrong")
+		return err
 	}
-	info, _ := backup.Stat()
-	backupBucket.PutReader("influx-"+randName(10), backup, info.Size(), "application/octet-stream", s3.PublicRead)
-
-	err = os.Remove(info.Name())
-	if err != nil {
-		log.Fatal(err)
-	}
+	bucket.PutReader(uploadName, content, info.Size(), "application/octet-stream", s3.PublicRead)
+	return nil
 }
 
 // randName returns a random string of letters of a given length
@@ -51,9 +95,9 @@ func randName(n int) string {
 	return string(b)
 }
 
-// triggerBackup invokes the influxd command in backup mode, returning a handle
+// captureBackup invokes the influxd command in backup mode, returning a handle
 // to the backup file
-func triggerBackup() (backup *os.File, err error) {
+func captureBackup() (backup *os.File, err error) {
 	influxBackup := exec.Command("influxd", "backup", "-host", influxHost+":"+influxPort, "influx-snapshot")
 
 	out, err := influxBackup.Output()
